333f7903276e55da8f785987eb12ad9f603f2b10
Kim Hak Kyeol <k.mark.kr@gmail.com>
2017-05-03 12:58:19 +0900

    add icon of form.

--- iMacPriceBot/iMacPriceBot.Designer.cs
+++ iMacPriceBot/iMacPriceBot.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(iMacPriceBot));
             this.SuspendLayout();
             // 
             // iMacPriceBot
@@ -35,6 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "iMacPriceBot";
             this.Text = "iMacPriceBot";
             this.ResumeLayout(false);
