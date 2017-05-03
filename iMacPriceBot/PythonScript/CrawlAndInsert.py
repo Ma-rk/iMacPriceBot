06fd4b93e9ea0784418a674766dc0f254f06f690
Kim Hak Kyeol <k.mark.kr@gmail.com>
2017-05-03 11:05:13 +0900

    refacotring.

--- iMacPriceBot/PythonScript/CrawlAndInsert.py
+++ iMacPriceBot/PythonScript/CrawlAndInsert.py
@@ -13,56 +13,64 @@ URL_CURRNCY_RATE = "https://api.manana.kr/exchange/rate.json"
 CURRNCY_RATE_JSON = str(urlopen(URL_CURRNCY_RATE).read().decode('utf8'))
 JSON_LIST = json.loads(CURRNCY_RATE_JSON)
 
-for dom_element in HTML_BODY_KR.find_all('ul', {'class': "as-macbundle-modelspecs"}):
-    dom_element_text = dom_element.text
-    dom_element_text_list = dom_element_text.split('\n')
-    del dom_element_text_list[7]
-    del dom_element_text_list[6]
-    del dom_element_text_list[3]
-    del dom_element_text_list[0]
 
-    idx = 0
-    while idx < len(dom_element_text_list):
-        dom_element_text_list[idx] = dom_element_text_list[idx].replace(' 프로세서', '')
-        dom_element_text_list[idx] = dom_element_text_list[idx].replace(' 드라이브1', '')
-        dom_element_text_list[idx] = dom_element_text_list[idx].replace(' 드라이브 1', '')
-        dom_element_text_list[idx] = dom_element_text_list[idx].replace(' 비디오 메모리', '')
-        idx += 1
+def get_imac_spec_kr():
+    for dom_element in HTML_BODY_KR.find_all('ul', {'class': "as-macbundle-modelspecs"}):
+        dom_element_text = dom_element.text
+        dom_element_text_list = dom_element_text.split('\n')
+        del dom_element_text_list[7]
+        del dom_element_text_list[6]
+        del dom_element_text_list[3]
+        del dom_element_text_list[0]
 
-    IMAC_SPEC_LIST.append(dom_element_text_list)
+        idx = 0
+        while idx < len(dom_element_text_list):
+            dom_element_text_list[idx] = dom_element_text_list[idx].replace(' 프로세서', '')
+            dom_element_text_list[idx] = dom_element_text_list[idx].replace(' 드라이브1', '')
+            dom_element_text_list[idx] = dom_element_text_list[idx].replace(' 드라이브 1', '')
+            dom_element_text_list[idx] = dom_element_text_list[idx].replace(' 비디오 메모리', '')
+            idx += 1
 
-print(IMAC_SPEC_LIST)
+        IMAC_SPEC_LIST.append(dom_element_text_list)
 
-i = 0
-for dom_element in HTML_BODY_KR.find_all('span', {'class': "current_price"}):
-    dom_element_text = dom_element.text
-    dom_element_text = dom_element_text.replace('￦', '')
-    dom_element_text = dom_element_text.replace(' ', '')
-    dom_element_text = dom_element_text.replace('\n', '')
-    dom_element_text = dom_element_text.replace(',', '')
-    IMAC_SPEC_LIST[i].append(dom_element_text)
-    i += 1
 
-print(IMAC_SPEC_LIST)
+def get_imac_price_kr():
+    i = 0
+    for dom_element in HTML_BODY_KR.find_all('span', {'class': "current_price"}):
+        dom_element_text = dom_element.text
+        dom_element_text = dom_element_text.replace('￦', '')
+        dom_element_text = dom_element_text.replace(' ', '')
+        dom_element_text = dom_element_text.replace('\n', '')
+        dom_element_text = dom_element_text.replace(',', '')
+        IMAC_SPEC_LIST[i].append(dom_element_text)
+        i += 1
 
-i = 0
-for dom_element in HTML_BODY_JP.find_all('span', {'class': "current_price"}):
-    dom_element_text = dom_element.text
-    dom_element_text = dom_element_text.replace('￥', '')
-    dom_element_text = dom_element_text.replace('(税別)', '')
-    dom_element_text = dom_element_text.replace(' ', '')
-    dom_element_text = dom_element_text.replace('\n', '')
-    dom_element_text = dom_element_text.replace(',', '')
-    IMAC_SPEC_LIST[i].append(dom_element_text)
-    i += 1
 
-print(IMAC_SPEC_LIST)
+def get_imac_price_jp():
+    i = 0
+    for dom_element in HTML_BODY_JP.find_all('span', {'class': "current_price"}):
+        dom_element_text = dom_element.text
+        dom_element_text = dom_element_text.replace('￥', '')
+        dom_element_text = dom_element_text.replace('(税別)', '')
+        dom_element_text = dom_element_text.replace(' ', '')
+        dom_element_text = dom_element_text.replace('\n', '')
+        dom_element_text = dom_element_text.replace(',', '')
+        IMAC_SPEC_LIST[i].append(dom_element_text)
+        i += 1
 
-print(JSON_LIST)
-RATE = 0
-for json_line in JSON_LIST:
-    if json_line['name'] == "JPYKRW=X":
-        RATE = float(json_line['rate'])
-        break
 
-print(RATE)
+def get_jpy_rate():
+    for json_line in JSON_LIST:
+        if json_line['name'] == "JPYKRW=X":
+            rate = float(json_line['rate'])
+            return rate
+
+
+if __name__ == '__main__':
+    get_imac_spec_kr()
+    get_imac_price_kr()
+    get_imac_price_jp()
+    for spec_line in IMAC_SPEC_LIST:
+        print(spec_line)
+    rate = get_jpy_rate()
+    print(rate)
