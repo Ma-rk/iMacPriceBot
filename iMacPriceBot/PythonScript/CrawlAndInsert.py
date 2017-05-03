816a1dd03155e0c013d01a7201add83c159dd6a3
Kim Hak Kyeol <k.mark.kr@gmail.com>
2017-05-03 09:52:29 +0900

    add function: get iMac spec(ko-kr)

---
+++ iMacPriceBot/PythonScript/CrawlAndInsert.py
@@ -0,0 +1,27 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+URL_KR = "https://www.apple.com/kr/shop/buy-mac/imac"
+HTML_BODY_KR = BeautifulSoup(urlopen(URL_KR).read(), "html.parser")
+
+IMAC_SPEC_LIST = []
+
+for dom_element in HTML_BODY_KR.find_all('ul', {'class': "as-macbundle-modelspecs"}):
+    dom_element_text = dom_element.text
+    dom_element_text_list = dom_element_text.split('\n')
+    del dom_element_text_list[7]
+    del dom_element_text_list[6]
+    del dom_element_text_list[3]
+    del dom_element_text_list[0]
+
+    idx = 0
+    while idx < len(dom_element_text_list):
+        dom_element_text_list[idx] = dom_element_text_list[idx].replace(' 프로세서', '')
+        dom_element_text_list[idx] = dom_element_text_list[idx].replace(' 드라이브1', '')
+        dom_element_text_list[idx] = dom_element_text_list[idx].replace(' 드라이브 1', '')
+        dom_element_text_list[idx] = dom_element_text_list[idx].replace(' 비디오 메모리', '')
+        idx += 1
+
+    IMAC_SPEC_LIST.append(dom_element_text_list)
+
+print(IMAC_SPEC_LIST)
