fedcede1a6feacd30eb89ce45983a0080ae3b439
Kim Hak Kyeol <k.mark.kr@gmail.com>
2017-05-03 12:54:08 +0900

    change class name.

--- iMacPriceBot/Program.cs
+++ iMacPriceBot/Program.cs
@@ -16,7 +16,7 @@ namespace iMacPriceBot
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new iMacPriceBot());
         }
     }
 }
--- iMacPriceBot/iMacPriceBot.Designer.cs
+++ iMacPriceBot/iMacPriceBot.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace iMacPriceBot
 {
-    partial class Form1
+    partial class iMacPriceBot
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // iMacPriceBot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "iMacPriceBot";
+            this.Text = "iMacPriceBot";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
